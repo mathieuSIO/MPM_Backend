547852f4b4306f5d76912a6257840288188e007f
MathieuCarvajal <mathieu.carvajal@yahoo.fr>
2026-04-19 23:14:09 +0200

    add simple route on devis

---
+++ src/routes/devisRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/", );//route pour créer mon devis
+router.get("/:id", );//route pour recuperer un devis specifique
+
+export default router;
